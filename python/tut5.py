5e3830c4196e5220e79eb8ceb9b397514bd3a5ab
santr4 <santra.aaranyak16092003@gmail.com>
2023-08-17 23:34:50 +0530

    classes and objects in oops

--- python/tut5.py
+++ python/tut5.py
@@ -1,17 +1,38 @@
 # ** oops in python - classes and objects
 
-class Person:
-    name = "surya"
-    age = 8
-    salary = 100000
+# class Person:
+#     name = "surya"
+#     age = 8
+#     salary = 100000
 
-a = Person()
-a.name = "aaranyak santra"
-a.age = 20
-a.salary = 10000000
-print(a.name)
-print(a.age)
-print(a.salary)
+# a = Person()
+# a.name = "aaranyak santra"
+# a.age = 20
+# a.salary = 10000000
+# print(a.name)
+# print(a.age)
+# print(a.salary)
 
-b = Person()
-print(b.name)   # a and b are the objects of the class Person.
+# b = Person()
+# print(b.name)   # a and b are the objects of the class Person.
+
+# *! self ka matlab vo object jiske liye ye method call kia ja raha hai
+
+class startup:
+    ceo = "ankush"
+    name = "cisco"
+    employees_no = 1000
+    funding = 500
+    office = 5
+    def info(self):
+        print(f"{self.ceo} is the ceo of the company: {self.name}")
+
+a = startup()
+a.ceo = "aaranyak"
+a.name = "AImankind"
+a.info()
+
+b = startup()
+b.ceo = "shreya"
+b.name = "AIunleash"
+b.info()
