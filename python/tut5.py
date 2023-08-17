c7fc3130cea50c682c9cbb6d3da1cca951f6b7bf
santr4 <santra.aaranyak16092003@gmail.com>
2023-08-17 23:25:36 +0530

    classes and objects in oops

---
+++ python/tut5.py
@@ -0,0 +1,17 @@
+# ** oops in python - classes and objects
+
+class Person:
+    name = "surya"
+    age = 8
+    salary = 100000
+
+a = Person()
+a.name = "aaranyak santra"
+a.age = 20
+a.salary = 10000000
+print(a.name)
+print(a.age)
+print(a.salary)
+
+b = Person()
+print(b.name)   # a and b are the objects of the class Person.
