19a0d54efdcaea12d955f2235c3efa719b3adf67
santr4 <santra.aaranyak16092003@gmail.com>
2023-08-17 23:37:39 +0530

    classes and objects in oops

--- python/tut5.py
+++ python/tut5.py
@@ -1,20 +1,20 @@
 # ** oops in python - classes and objects
 
-# class Person:
-#     name = "surya"
-#     age = 8
-#     salary = 100000
+class Person:
+    name = "surya"
+    age = 8
+    salary = 100000
 
-# a = Person()
-# a.name = "aaranyak santra"
-# a.age = 20
-# a.salary = 10000000
-# print(a.name)
-# print(a.age)
-# print(a.salary)
+a = Person()
+a.name = "aaranyak santra"
+a.age = 20
+a.salary = 10000000
+print(a.name)
+print(a.age)
+print(a.salary)
 
-# b = Person()
-# print(b.name)   # a and b are the objects of the class Person.
+b = Person()
+print(b.name)   # a and b are the objects of the class Person.
 
 # *! self ka matlab vo object jiske liye ye method call kia ja raha hai
 
